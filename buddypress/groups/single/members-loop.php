a63f02945570318b0e4494efa5c001d5f599d8eb
Robert Gilman <reg@context.org>
2025-09-19 10:13:29 -0700

    Upgrades group member display
    Always includes the grid of avatars at the top and alphabetizes lists by first name.

--- buddypress/groups/single/members-loop.php
+++ buddypress/groups/single/members-loop.php
@@ -22,7 +22,7 @@ $is_follow_active = bp_is_active('activity') && function_exists('bp_is_activity_
 $follow_class = $is_follow_active ? 'follow-active' : '';
 ?>
 
-<?php if ( bp_group_has_members( bp_ajax_querystring( 'group_members' ) . '&type=active&per_page=40' ) ) : ?>
+<?php if ( bp_group_has_members( bp_ajax_querystring( 'group_members' ) . '&type=alphabetical&per_page=40' ) ) : ?>
 
 	<?php bp_nouveau_group_hook( 'before', 'members_content' ); ?>
 
@@ -33,6 +33,53 @@ $follow_class = $is_follow_active ? 'follow-active' : '';
 
 	<?php
 	global $bfc_dropdown_prefix;
+	?>
+	<ul id="members-list" class="<?php bp_nouveau_loop_classes(); ?>">
+		<?php
+		$bfc_dropdown_prefix = 'lg';
+		while ( bp_group_members() ) :
+			bp_group_the_member();
+			$member_id           = bp_get_member_user_id();
+			$show_message_button = buddyboss_theme()->buddypress_helper()->buddyboss_theme_show_private_message_button( $member_id, bp_loggedin_user_id() );
+			//Check if members_list_item has content
+			ob_start();
+			bp_nouveau_member_hook( '', 'members_list_item' );
+			$members_list_item_content = ob_get_contents();
+			ob_end_clean();
+			$member_loop_has_content = empty($members_list_item_content) ? false : true;
+			$type = $bfc_dropdown_prefix . '-group-member';
+			$source = bp_get_member_user_id();
+			$person = $source;
+			?>
+			<li <?php bp_member_class( array( 'item-entry' ) ); ?> data-bp-item-id="<?php echo esc_attr( bp_get_group_member_id() ); ?>" data-bp-item-component="members">
+				<div class="list-wrap <?php echo $footer_buttons_class; ?> <?php echo $follow_class; ?> <?php echo $member_loop_has_content ? ' has_hook_content' : ''; ?>">
+					<div class="item-avatar">
+					<span data-toggle="<?php echo $type . '-dropdown-' . $source ; ?>">
+					<?php 
+						bb_user_status( bp_get_member_user_id() );
+						bp_member_avatar( bp_nouveau_avatar_args() );?></span>
+					<?php echo bfc_member_dropdown( $type, $source, $person, $follow_class );?>
+					<span class="list-title member-name"><?php bp_member_name(); ?></span>
+						
+					</div>
+
+					<div class="bp-members-list-hook">
+					<?php 
+						if($member_loop_has_content){ ?>
+							<a class="more-action-button" href="#"><i class="bb-icon-menu-dots-h"></i></a>
+						<?php } ?>
+						<div class="bp-members-list-hook-inner">
+							<?php bp_nouveau_member_hook( '', 'members_list_item' ); ?>
+						</div>
+					</div>
+				</div>
+			</li>
+
+		<?php 
+			endwhile;
+			?>
+	</ul>
+	<?php
 	$count = groups_get_current_group()->total_member_count;
 	// We use 21 as the break point between groups that feel "small" and those that feel "large". This page always displays
 	// the group members' avatars and names. For small groups, it also displays their self intros.
@@ -81,50 +128,7 @@ $follow_class = $is_follow_active ? 'follow-active' : '';
 		<?php 
 
 		endwhile; 
-		else :?>
-		<ul id="members-list" class="<?php bp_nouveau_loop_classes(); ?>">
-		<?php
-		$bfc_dropdown_prefix = 'lg';
-		while ( bp_group_members() ) :
-			bp_group_the_member();
-			$member_id           = bp_get_member_user_id();
-			$show_message_button = buddyboss_theme()->buddypress_helper()->buddyboss_theme_show_private_message_button( $member_id, bp_loggedin_user_id() );
-			//Check if members_list_item has content
-			ob_start();
-			bp_nouveau_member_hook( '', 'members_list_item' );
-			$members_list_item_content = ob_get_contents();
-			ob_end_clean();
-			$member_loop_has_content = empty($members_list_item_content) ? false : true;
-			$type = $bfc_dropdown_prefix . '-group-member';
-			$source = bp_get_member_user_id();
-			$person = $source;
-			?>
-			<li <?php bp_member_class( array( 'item-entry' ) ); ?> data-bp-item-id="<?php echo esc_attr( bp_get_group_member_id() ); ?>" data-bp-item-component="members">
-				<div class="list-wrap <?php echo $footer_buttons_class; ?> <?php echo $follow_class; ?> <?php echo $member_loop_has_content ? ' has_hook_content' : ''; ?>">
-					<div class="item-avatar">
-					<span data-toggle="<?php echo $type . '-dropdown-' . $source ; ?>">
-					<?php 
-						bb_user_status( bp_get_member_user_id() );
-						bp_member_avatar( bp_nouveau_avatar_args() );?></span>
-					<?php echo bfc_member_dropdown( $type, $source, $person, $follow_class );?>
-					<span class="list-title member-name"><?php bp_member_name(); ?></span>
-						
-					</div>
-
-					<div class="bp-members-list-hook">
-					<?php 
-						if($member_loop_has_content){ ?>
-							<a class="more-action-button" href="#"><i class="bb-icon-menu-dots-h"></i></a>
-						<?php } ?>
-						<div class="bp-members-list-hook-inner">
-							<?php bp_nouveau_member_hook( '', 'members_list_item' ); ?>
-						</div>
-					</div>
-				</div>
-			</li>
-
-		<?php 
-			endwhile;
+	
 		endif;
 		?>
 
